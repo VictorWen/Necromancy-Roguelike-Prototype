0c9b700291dc0ce73bc17fc089b6926dbeb8eea4
Victor Wen <vicwen02@g.ucla.edu>
2021-11-02 13:58:18 -0700

    Basic player controller

---
+++ Necromancy Roguelike Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int Health { get { return health; } }
+
+    [SerializeReference] private int health = 10;
+    [SerializeReference] private float movementSpeed = 5;
+
+    private SpriteRenderer sprite;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        float hori = Input.GetAxisRaw("Horizontal");
+        float vert = Input.GetAxisRaw("Vertical");
+        
+        // Move
+        if (hori != 0 || vert != 0)
+        {
+            Vector3 direction = new Vector3(hori, vert);
+            direction /= Vector3.Distance(direction, new Vector3(0, 0));
+            transform.position += direction * movementSpeed * Time.deltaTime;
+        }
+
+        // Flip sprite on direction change
+        if (hori > 0 && sprite.flipX)
+            sprite.flipX = false;
+        else if (hori < 0 && !sprite.flipX)
+            sprite.flipX = true;
+    }
+}
